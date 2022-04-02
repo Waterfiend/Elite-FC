d9fdb1f7fbae9377f5e0169fb80b34e06d4193f5
Waterfiend <Waterfiend@users.noreply.github.com>
2022-04-02 23:00:57 +0300

    Add, Edit, Delete Matches

--- system/models.py
+++ system/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+
+
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=255)
@@ -9,9 +11,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
-    
+
     def get_absolute_url(self):
-        return reverse('article-detail', args = (str(self.id)) )
+        return reverse('article-detail', args=(str(self.id)))
+
 
 class User(models.Model):
     id = models.IntegerField(primary_key=True)
@@ -22,3 +25,14 @@ class User(models.Model):
     date_of_birth = models.TextField()
     fan_tier = models.TextField()
     role = models.TextField(default='fan')
+
+
+class Match(models.Model):
+    id = models.IntegerField(primary_key=True)
+    date = models.TextField(default="XX/XX/XX")
+    time = models.TextField(default="XX:XX")
+    team1 = models.TextField(default="")
+    team2 = models.TextField(default="")
+    score1 = models.IntegerField(default=0)
+    score2 = models.IntegerField(default=0)
+    location = models.TextField(default="")
--- system/urls.py
+++ system/urls.py
@@ -20,4 +20,7 @@ urlpatterns = [
     path('editUserValidate/<int:id>', views.editUserValidate),
     path('deleteUser/<int:id>', views.deleteUser),
     path('schedule/', views.display_schedule),
+    path('deletematch/<int:match_id>', views.delete_match),
+    path('editmatch/<int:match_id>', views.display_matchform),
+    path('postmatch/<int:match_id>', views.post_match),
 ]
